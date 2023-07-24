2e143d384978b533e951b36465c3b7b5d456e295
osman corluk <corluk@hotmail.com>
2023-07-24 19:39:36 +0300

    d

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,6 @@
 import React from "react"; 
 import { createBrowserRouter } from "react-router-dom";
-import IndexPage from "../pages/index/index"
+import IndexPage from "./pages/index/index"
 import Routes from "./routes"
 const router = createBrowserRouter(Routes)
 
